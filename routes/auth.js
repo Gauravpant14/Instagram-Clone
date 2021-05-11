f04df9ad7260a41cd65a64a3ee029e18bd67a525
Gaurav <aadeezcity@gmail.com>
2021-05-11 21:13:48 +0530

    jsonwebtoken has been used for generating token

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./../Keys");
 
+//sign-up route
 router.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
 
@@ -38,6 +41,33 @@ router.post("/signup", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+//sign-in route
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "please add email or password" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid Email or password" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          // res.json({ message: "Succesfully signed in" });
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+          res.json({ token });
+        } else {
+          return res.status(422).json({ error: "Invalid Email or password" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;
